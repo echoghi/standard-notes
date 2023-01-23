d4586336b6afab20461e0fdf26e6f41397755d18
Emile Choghi <emilechoghi@protonmail.com>
2023-01-23 13:38:36 -0800

    fix: html lang

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en'
+    }
 });
